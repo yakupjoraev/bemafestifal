dd8d2aae38f15c2da727fdd2adf0c4f2b032faf9
Yakup <yakupjoraev@gmail.com>
2023-06-23 16:57:48 +0300

    added modals and sections

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -3,6 +3,12 @@
 
 // Мобильное меню бургер
 function burgerMenu() {
+  const container = document.querySelector('.nav')
+
+  if (!container) {
+    return null
+  }
+
   const burger = document.querySelector('.burger')
   const menu = document.querySelector('.menu')
   const body = document.querySelector('body')
@@ -172,4 +178,31 @@ function toggleAccordion() {
 
 
 accordionItems.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+
+const openModalBtns = document.querySelectorAll('.open-modal-btn');
+const closeModalBtns = document.querySelectorAll('.close-modal-btn');
+const modals = document.querySelectorAll('.modal');
+
+openModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modalId = btn.dataset.modalId;
+    const modal = document.getElementById(modalId);
+    modal.classList.add('show');
+  });
+});
+
+closeModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modal = btn.closest('.modal');
+    modal.classList.remove('show');
+  });
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target.classList.contains('modal')) {
+    event.target.classList.remove('show');
+  }
+});
 
